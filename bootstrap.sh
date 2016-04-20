be6f5a1030c7ed82ae4ddac50b5f7ed074d54e7a
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2016-04-20 10:55:57 -0400

    Move node and npm setup to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,8 @@ export TMP_DIR=/tmp
 export NVM_DIR=/opt/nvm
 export PROFILE=$HOME/.profile
 export DEMETEORIZER_VERSION=3.1.0
+export NODE_VERSION=0.10.41
+export NPM_VERSION=3.8.6
 
 # Create $HOME/.profile and export environment variable
 if [[ ! -d $HOME ]]; then
@@ -39,9 +41,10 @@ cd /opt
 # Install nvm
 mkdir -p $NVM_DIR
 curl https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+source $PROFILE
 
 # Ensure mop can use nvm, but not write to it
-chown mop:mop /opt/nvm/nvm.sh
+chown -R mop:mop /opt/nvm/nvm.sh
 chmod g-w /opt/nvm/nvm.sh
 
 # Install get-version
@@ -49,6 +52,15 @@ npm install -g get-version
 
 # Install demeteorizer
 npm install -g demeteorizer@$DEMETEORIZER_VERSION
+
+# Setup NPM and Node versions
+printf "Installing node $NODE_VERSION\n"
+nvm install $NODE_VERSION > /dev/null 2>&1
+
+printf "Installing npm $NPM_VERSION\n"
+npm install npm@$NPM_VERSION --global > /dev/null 2>&1
+
+nvm alias deploy $(nvm current)
 
 # Install Meteor
 curl https://install.meteor.com/ | sh
